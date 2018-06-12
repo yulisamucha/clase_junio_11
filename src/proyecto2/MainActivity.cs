80e70ad53ae2c540213eeb45af6233d095699129
Yuli <i1520761@continental.edu.pe>
2018-06-11 20:09:36 -0500

    tarea 06

---
+++ src/proyecto2/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace proyecto2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnAuth;
+        EditText Nombre;
+        EditText Contra;
+        EditText Token;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnAuth = FindViewById<Button>(Resource.Id.btnAuth);
+
+            Nombre = FindViewById<EditText>(Resource.Id.Nombre);
+            Contra = FindViewById<EditText>(Resource.Id.Contra);
+            Token = FindViewById<EditText>(Resource.Id.Token);
+
+            btnAuth.Click += DoAuthentication;
+        }
+           
+        private void DoAuthentication(object sender, EventArgs e)
+        {
+            //calcular valor del idtext
+            if (Nombre.Text.Equals("bichito") && Contra.Text.Equals("123456") && Token.Text.Equals("987654"))
+            {
+                Toast.MakeText(this, "usuario valido", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "usuario invalido", ToastLength.Short).Show();
+            }
+           
+        }
+    }
+}
+
---
+++ src/proyecto2/SplashScrean.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace proyecto2
+{
+    [Activity(Label = "SplashScream", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScrean : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Desabilitamos que el usuario vuelva atraz
+        public override void OnBackPressed() { }
+
+        // Espera 8 segundos
+        async void SimulateStartup()
+        {
+            //Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(8000);
+            //Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
